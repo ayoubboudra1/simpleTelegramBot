65d178fd906e0de437a33aeddec638dd8abe9c7f
ayoubboudra1 <ayoub.boudra1@gmail.com>
2024-07-31 19:20:36 +0100

    my bot

--- bot.js
+++ bot.js
@@ -2,58 +2,88 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { count } = require('console');
 
 const token  = '6665225787:AAHk7YHyQjHqORIdoEJ6WFDLzGRXNd0qTRI';
 const chatId = '1764251980';
 const url    = 'https://trouverunlogement.lescrous.fr/tools/36/search?bounds=5.2286902_43.3910329_5.5324758_43.1696205'
+// const url    = 'https://trouverunlogement.lescrous.fr/tools/36/search'
 
 
 const words = ["BALUSTRES", "CHATENOUD" ];
 
 const bot = new TelegramBot(token, { polling: true });
 
+var current_number = 0;
 
 
-function monitorTemplate() {
+var counter = 0;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function monitorTemplate() {
     // let lastModifiedTime = fs.statSync(filePath).mtimeMs;
 
-    setInterval(async () => {
 
-        try{
-            const { data } = await axios.get(url);
-            const $ = cheerio.load(data);
+    
+        // setInterval(async () => {
+        while(true){    
 
-            var numberOfMessage = 50;
-            let adresse = $('p.fr-card__desc').text();
-            let prix    = $('p.fr-badge').text();
-            let surface = $('p.fr-card__detail').text().split('m²')[0].trim();
-            let link = $('h3.fr-card__title > a').attr('href');
-            let title  = $('h3.fr-card__title > a').text();
-
-
-            const message = `Adresse : ${adresse}\n\nPrix : ${prix}\n\nSurface : ${surface}\n\nLink : https://trouverunlogement.lescrous.fr${link}\n\nTitle : ${title}`
-
-            bot.sendMessage(chatId, message);
             
+            try{
+                const { data } = await axios.get(url);
+                const $ = cheerio.load(data);
+                counter +=1;
+                
+                list_of_carts = $('ul.fr-grid-row.fr-grid-row--gutters.svelte-11sc5my')
+                if(current_number < list_of_carts.length) {
+                    list_of_carts.each((index, element) => {
+                        const $ = cheerio.load(element);
+                        let adresse = $('p.fr-card__desc').text();
+                        let prix    = $('p.fr-badge').text();
+                        let surface = $('p.fr-card__detail').text().split('m²')[0].trim();
+                        let link = $('h3.fr-card__title > a').attr('href');
+                        let title  = $('h3.fr-card__title > a').text();
+                        const message = `Adresse : ${adresse}\n\nPrix : ${prix}\n\nSurface : ${surface}\n\nLink : https://trouverunlogement.lescrous.fr${link}\n\nTitle : ${title}`
+                        bot.sendMessage(chatId, message);
+                        // console.log(message)
+                    })
+    
+                    for(let i = 0; i < 50; i++) {
+                         // Wait 10 seconds
+                        await sleep(2000)
+                        bot.sendMessage(chatId, "Wake up !");
+    
+    
+                    }
+    
+                }
+    
+                current_number = list_of_carts.length
 
-            const containsWord = words.some(word => title.includes(word));
-
-            if(!containsWord) {
-                numberOfMessage = 2
+                if(counter % 200 == 0) {
+                    bot.sendMessage(chatId, "Still Working",{disable_notification: true});
+                }
+    
+    
+               
             }
-            for(let i = 0; i < numberOfMessage; i++) {
-                setTimeout(() => {
-                    bot.sendMessage(chatId, message);
-                }, 1000);
+            catch(err) {
+                console.log(err);
             }
-           
-        }
-        catch(err) {
-            console.log(err);
+
+            await sleep(20000);
+
+            // console.log("20sec later,new turn end")
+        // }, 30000);
         }
 
-    }, 10000);
+   
 }
 
 // checkWebsite(url, 'h1');
+
 monitorTemplate();
+
