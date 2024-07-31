ef37f4b80eaa0028bb755c09ea070cd4bfcf4b7c
ayoubboudra1 <ayoub.boudra1@gmail.com>
2024-07-31 17:38:51 +0100

    my bot

--- bot.js
+++ bot.js
@@ -1,24 +1,59 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const token = '6665225787:AAHk7YHyQjHqORIdoEJ6WFDLzGRXNd0qTRI';
+const token  = '6665225787:AAHk7YHyQjHqORIdoEJ6WFDLzGRXNd0qTRI';
 const chatId = '1764251980';
+const url    = 'https://trouverunlogement.lescrous.fr/tools/36/search?bounds=5.2286902_43.3910329_5.5324758_43.1696205'
 
 
-
+const words = ["BALUSTRES", "CHATENOUD" ];
 
 const bot = new TelegramBot(token, { polling: true });
 
-function monitorTemplate(filePath) {
+
+
+function monitorTemplate() {
     // let lastModifiedTime = fs.statSync(filePath).mtimeMs;
 
-    setInterval(() => {
-        // const stats = fs.statSync(filePath);
-        // if (stats.mtimeMs !== lastModifiedTime) {
-        // lastModifiedTime = stats.mtimeMs;
-        bot.sendMessage(chatId, `Template has changed at `);
-        // }
+    setInterval(async () => {
+
+        try{
+            const { data } = await axios.get(url);
+            const $ = cheerio.load(data);
+
+            var numberOfMessage = 50;
+            let adresse = $('p.fr-card__desc').text();
+            let prix    = $('p.fr-badge').text();
+            let surface = $('p.fr-card__detail').text().split('m²')[0].trim();
+            let link = $('h3.fr-card__title > a').attr('href');
+            let title  = $('h3.fr-card__title > a').text();
+
+
+            const message = `Adresse : ${adresse}\n\nPrix : ${prix}\n\nSurface : ${surface}\n\nLink : https://trouverunlogement.lescrous.fr${link}\n\nTitle : ${title}`
+
+            bot.sendMessage(chatId, message);
+            
+
+            const containsWord = words.some(word => title.includes(word));
+
+            if(containsWord) {
+                numberOfMessage = 2
+            }
+            for(let i = 0; i < numberOfMessage; i++) {
+                setTimeout(() => {
+                    bot.sendMessage(chatId, message);
+                }, 1000);
+            }
+           
+        }
+        catch(err) {
+            console.log(err);
+        }
+
     }, 5000);
 }
 
-monitorTemplate('path/to/your/template/file.html');
+// checkWebsite(url, 'h1');
+monitorTemplate();
